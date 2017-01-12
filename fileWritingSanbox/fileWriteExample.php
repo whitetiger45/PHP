1f7971073cf6a4b794edf72badbba35025927b3d
Bryan <kanubu@mail.uc.edu>
2017-01-12 18:21:20 -0500

    added command line read

--- fileWritingSanbox/fileWriteExample.php
+++ fileWritingSanbox/fileWriteExample.php
@@ -3,7 +3,8 @@ $file = 'people.txt';
 // Open the file to get existing content
 $current = file_get_contents($file);
 // Append a new person to the file
-$current = "Goodbye World!\n";
+$line = readline("Enter your message to save to file: ");
+// $current = "Goodbye World!\n";
 // Write the contents back to the file
-file_put_contents($file, $current);
+file_put_contents($file, $line);
 ?>
