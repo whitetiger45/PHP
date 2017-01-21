5fa308ae8a2a4e635ffb788c680e315d02aa6b0e
Bryan <kanubu@mail.uc.edu>
2017-01-20 23:21:42 -0500

    commented out line reading original file

--- fileWritingSanbox/fileWriteExample.php
+++ fileWritingSanbox/fileWriteExample.php
@@ -1,7 +1,7 @@
 <?php
 $file = 'people.txt';
 // Open the file to get existing content
-$current = file_get_contents($file);
+// $current = file_get_contents($file);
 // Append a new person to the file
 $line = readline("Enter your message to save to file: ");
 // $current = "Goodbye World!\n";
